f6e79134c11e8d1270c35e0c7dc19470939c9383
fanse2 <fanse2@gmail.com>
2024-03-06 14:48:02 -0800

    uploaded paper

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const app = Vue.createApp({
     data(){
         return {
             title: 'Korean News Las Vegas',
+            newsList: []
         }
-    }
+    },
+    mounted(){
+        fetch('http://kvegas.site/man/link.json.php')
+        .then(res=> res.json())
+        .then(json=>{
+            console.log(json)
+            this.newsList = json
+        })
+    },
+
 })
 
 app.mount("#app")
